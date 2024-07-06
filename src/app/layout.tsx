c0ceaa85fe017eda8f2f26dd0357b6a67f2c0fd8
MuffinTheDragon <dhaval.malhotra@hotmail.com>
2024-07-06 17:55:08 -0400

    Add vercel analytics and insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +29,8 @@ export default function RootLayout({
 				>
 					{children}
 					<Toaster richColors />
+					<SpeedInsights />
+					<Analytics />
 				</ThemeProvider>
 			</body>
 		</html>
