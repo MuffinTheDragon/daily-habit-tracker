9623d47e2b3617746b437876905b651b080da062
MuffinTheDragon <dhaval.malhotra@hotmail.com>
2024-07-11 17:15:36 -0400

    test: fix tests

--- src/__tests__/isHabitDoneForToday.test.tsx
+++ src/__tests__/isHabitDoneForToday.test.tsx
@@ -1,25 +1,48 @@
+import { HabitType } from "@/data/HabitType";
 import { getCurrentDate, isHabitDoneForToday } from "@/lib/utils";
-import { addDays } from "date-fns";
-import { expect, test } from "vitest";
+import { addDays, startOfDay } from "date-fns";
+import { beforeEach, expect, test } from "vitest";
 
 const now = getCurrentDate();
+const year = now.getFullYear();
 
-test("Test with undefined date", () => {
-	const isDone = isHabitDoneForToday(undefined);
+let habit: HabitType;
+
+beforeEach(() => {
+	habit = {
+		id: "1",
+		created: new Date(),
+		name: "Test habit",
+		description: "",
+		streak: 0,
+		longestStreak: 0,
+		checks: 0,
+		archived: false,
+		archivedDate: null,
+		streakFreezes: 0,
+		graph: [{ year, daysChecked: [], manualDaysChecked: [] }],
+		longestStreakDateSet: now,
+	};
+});
+
+test("Test with new habit", () => {
+	const isDone = isHabitDoneForToday(habit);
 
 	expect(isDone).toBe(false);
 });
 
-test("Test where date is < today", () => {
-	const previous = addDays(now, -5);
+test("Test where habit was checked before today", () => {
+	habit.graph[0].daysChecked.push(addDays(now, -5));
 
-	const isDone = isHabitDoneForToday(previous);
+	const isDone = isHabitDoneForToday(habit);
 
 	expect(isDone).toBe(false);
 });
 
 test("Test where date is today", () => {
-	const isDone = isHabitDoneForToday(now);
+	habit.graph[0].daysChecked.push(startOfDay(now));
+
+	const isDone = isHabitDoneForToday(habit);
 
 	expect(isDone).toBe(true);
 });
