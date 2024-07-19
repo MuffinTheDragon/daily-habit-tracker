052bac2023b03f9de49b30465916c4722fad01df
MuffinTheDragon <dhaval.malhotra@hotmail.com>
2024-07-19 00:33:27 -0400

    chore: update pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
     dest: "public",
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
     fallbacks: {
         document: "/habits"
     }
