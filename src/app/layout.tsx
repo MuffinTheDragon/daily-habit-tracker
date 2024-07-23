8d288e582c1f7c95edabfd44e3ab33ab6b9bc2b1
MuffinTheDragon <dhaval.malhotra@hotmail.com>
2024-07-23 15:27:51 -0400

    chore: add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,22 @@ import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Daily habit tracker",
-	description: "Track your daily habits with this simple and efficient tool",
+	title: "Daily habit tracker - Free & Minimalist habit tracker",
+	description:
+		"Try this free habit tracker app with streaks, freezes to protect streaks, and a visual map to see consistency. Track your habits effortlessly!",
+	openGraph: {
+		type: "website",
+		url: "https://www.dailyhabits.dev",
+		images: [
+			"https://www.dailyhabits.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhabits-light.ee373f59.png&w=1080&q=75",
+		],
+		title: "Daily habit tracker - Free & Minimalist habit tracker",
+		description:
+			"Try this free habit tracker app with streaks, freezes to protect streaks, and a visual map to see consistency. Track your habits effortlessly!",
+	},
+	alternates: {
+		canonical: "https://www.dailyhabits.dev",
+	},
 };
 
 export default function RootLayout({
